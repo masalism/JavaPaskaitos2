ad305ed067f2229e037e0392829fd5ec487e4d0d
masalism <mantasmasalis@gmail.com>
2018-03-20 20:37:38 +0200

    first commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,32 @@
+public class MyFirstClass {
+    public static void main(String[] args) { //psvm - main klasei
+        System.out.println("Hello world"); //sout - spausdinimui
+          int a = 34;
+
+        //sukurtas objektas
+        MyFirstClass myFirstClass = new MyFirstClass();
+
+        //ne statinis kvieciamas statiniame per klases objekta
+        myFirstClass.myNotStaticMethod(a);
+
+        //tesiog per varda toje pacioje klaseje - statinio kvietimas
+        myStaticMethod();
+
+        float b = 4.6f;
+        double c = 2.4;
+
+        boolean d = true;//false
+
+        char e = 'A';
+
+        String f = "This is string";
+    }
+
+    private void myNotStaticMethod(int numb) {
+        System.out.println("Jusu skaicius " + numb);
+    }
+
+    private static void myStaticMethod(){
+        System.out.println("Cia yra statinis metodas");
+    }
+}
